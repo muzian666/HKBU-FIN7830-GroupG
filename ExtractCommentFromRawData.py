4914e1e17c93d4aa6510ec936b6a331a35603533
muzian666 <andylee_2001@163.com>
2023-11-10 12:21:29 +0800

    Adjustment of file structure

--- ExtractCommentFromRawData.py
+++ ExtractCommentFromRawData.py
@@ -2,7 +2,7 @@ import pandas as pd
 from docx import Document
 
 # Load the CSV file
-file_path = 'Uwants-離職原因-2023-11-07_18-42-13.csv'
+file_path = 'Resources/Data/WebData/Uwants-離職原因-2023-11-07_18-42-13.csv'
 
 # Attempt to read the CSV file with different encodings commonly used for Chinese text
 for encoding in ['utf-8', 'gbk', 'gb18030']:
@@ -23,5 +23,5 @@ for comment in comments:
     doc.add_paragraph(str(comment))
 
 # Save the document
-docx_file_path = 'extracted_comments.docx'
+docx_file_path = 'Resources/Data/WebData/extracted_comments.docx'
 doc.save(docx_file_path)
